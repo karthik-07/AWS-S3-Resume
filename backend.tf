d3e1125dc694f96e749354f31740b343307ce45e
karthik-07 <karthikvs216@gmail.com>
2024-08-10 08:26:57 -0400

    Added state file for tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-bucket-karthik"
-#     key    = "terraform-state/${TF_STATE_NAME}"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket-karthik"
+    key    = "terraform-state/${TF_STATE_NAME}"
+    region = "us-east-1"
+  }
+}
 
