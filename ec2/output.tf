cabfb4ae46cefa2e220ff6fc1fe797477a517260
karthik-07 <karthikvs216@gmail.com>
2024-08-12 11:39:55 -0400

    Changes in outputEC2

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,3 +1,14 @@
 output "ec2_public_ip_address" {
-  value = aws_instance.jenkins_ec2_instance.public_ip
+  description = "The public IP address of the EC2 instance"
+  value       = length(aws_instance.jenkins_ec2_instance) > 0 ? aws_instance.jenkins_ec2_instance[0].public_ip : null
+}
+
+output "ec2_private_ip_address" {
+  description = "The private IP address of the EC2 instance"
+  value       = length(aws_instance.jenkins_ec2_instance) > 0 ? aws_instance.jenkins_ec2_instance[0].private_ip : null
+}
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = length(aws_instance.jenkins_ec2_instance) > 0 ? aws_instance.jenkins_ec2_instance[0].id : null
 }
