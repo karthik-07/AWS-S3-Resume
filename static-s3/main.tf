cead1732f990d5c9cbac14661b341643af3f0794
karthik-07 <karthikvs216@gmail.com>
2024-08-15 18:47:59 -0400

    Small changes v1

--- static-s3/main.tf
+++ static-s3/main.tf
@@ -22,6 +22,23 @@ resource "aws_s3_bucket_website_configuration" "static-website-config" {
   }
 }
 
+resource "aws_s3_bucket_policy" "resume_bucket_policy" {
+  bucket = aws_s3_bucket.static-file-store-karthik.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.resume_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
 resource "aws_s3_bucket_public_access_block" "s3-bucket-access-block" {
   bucket = aws_s3_bucket.static-file-store-karthik.id
 
