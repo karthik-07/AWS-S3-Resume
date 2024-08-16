c1471af18a6cdbd439f14f41d26085569781d130
karthik-07 <karthikvs216@gmail.com>
2024-08-15 23:34:50 -0400

    Small changes v5

--- static-s3/main.tf
+++ static-s3/main.tf
@@ -42,8 +42,8 @@ resource "aws_s3_bucket_policy" "resume_bucket_policy" {
 resource "aws_s3_bucket_public_access_block" "s3-bucket-access-block" {
   bucket = aws_s3_bucket.static-file-store-karthik.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
